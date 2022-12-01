dda435865a0e8a3b402e0ffc1bbf67a343007748
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-01 09:07:51 +0200

    Time: 4 ms (43.12%), Space: 7.6 MB (78.25%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        if(nums.size() < 2) return nums[0];
+        
+        int previous = nums[0];
+        int current = max(nums[0], nums[1]);
+        
+        
+        for(int i = 2; i < nums.size(); i++){
+            int temp = previous;
+            previous = current;
+            current = max(temp + nums[i], current);
+        }
+        
+        return current;
+    }
+};
