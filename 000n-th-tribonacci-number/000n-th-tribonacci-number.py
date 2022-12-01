318e236f85d5f749c0541ad00099fcaaaa91b53a
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-01 09:26:31 +0200

    Time: 70 ms, Memory: 13.7 MB - LeetHub

--- 000n-th-tribonacci-number/000n-th-tribonacci-number.py
+++ 000n-th-tribonacci-number/000n-th-tribonacci-number.py
@@ -3,9 +3,14 @@ class Solution:
         if n == 0: return 0
         elif n == 1: return 1
         elif n == 2: return 1
-        dp = [0]*(n+1)
-        dp[1] = dp[2] = 1
+        dp1 = 0
+        dp2 = 1
+        dp3 = 1
         
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-        return dp[n]
+        for i in range(2, n):
+            ans = dp1 + dp2 + dp3
+            dp1 = dp2
+            dp2 = dp3
+            dp3 = ans
+        
+        return ans
