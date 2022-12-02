faa5e1155949b909e45c48439689cbddebc9cb67
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-02 10:11:08 +0200

    Time: 902 ms, Memory: 128.1 MB - LeetHub

---
+++ find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,29 @@
+class MedianFinder {
+    multiset<int> data;
+    multiset<int>::iterator mid;
+
+public:
+    MedianFinder()
+        : mid(data.end())
+    {
+    }
+
+    void addNum(int num)
+    {
+        const int n = data.size();
+        data.insert(num);
+
+        if (!n)                                 // first element inserted
+            mid = data.begin();
+        else if (num < *mid)                    // median is decreased
+            mid = (n & 1 ? mid : prev(mid));
+        else                                    // median is increased
+            mid = (n & 1 ? next(mid) : mid);
+    }
+
+    double findMedian()
+    {
+        const int n = data.size();
+        return ((double) *mid + *next(mid, n % 2 - 1)) * 0.5;
+    }
+};
