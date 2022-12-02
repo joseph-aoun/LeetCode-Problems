56c8daba9e81b3a982ce9f8c506804d761b4cd3b
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-02 10:07:13 +0200

    Time: 411 ms (79.39%), Space: 128.1 MB (7.40%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,48 +1,29 @@
 class MedianFinder {
     multiset<int> data;
-    multiset<int>::iterator lo_median, hi_median;
+    multiset<int>::iterator mid;
 
 public:
     MedianFinder()
-        : lo_median(data.end())
-        , hi_median(data.end())
+        : mid(data.end())
     {
     }
 
     void addNum(int num)
     {
-        const size_t n = data.size();   // store previous size
+        const int n = data.size();
+        data.insert(num);
 
-        data.insert(num);               // insert into multiset
-
-        if (!n) {
-            // no elements before, one element now
-            lo_median = hi_median = data.begin();
-        }
-        else if (n & 1) {
-            // odd size before (i.e. lo == hi), even size now (i.e. hi = lo + 1)
-
-            if (num < *lo_median)       // num < lo
-                lo_median--;
-            else                        // num >= hi
-                hi_median++;            // insertion at end of equal range
-        }
-        else {
-            // even size before (i.e. hi = lo + 1), odd size now (i.e. lo == hi)
-
-            if (num > *lo_median && num < *hi_median) {
-                lo_median++;                    // num in between lo and hi
-                hi_median--;
-            }
-            else if (num >= *hi_median)         // num inserted after hi
-                lo_median++;
-            else                                // num <= lo < hi
-                lo_median = --hi_median;        // insertion at end of equal range spoils lo
-        }
+        if (!n)                                 // first element inserted
+            mid = data.begin();
+        else if (num < *mid)                    // median is decreased
+            mid = (n & 1 ? mid : prev(mid));
+        else                                    // median is increased
+            mid = (n & 1 ? next(mid) : mid);
     }
 
     double findMedian()
     {
-        return ((double) *lo_median + *hi_median) * 0.5;
+        const int n = data.size();
+        return ((double) *mid + *next(mid, n % 2 - 1)) * 0.5;
     }
 };
