2be8bbfc113d1f30756555a4375bbbc646788569
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-04 14:42:06 +0200

    Time: 3067 ms (5.02%), Space: 29 MB (13.54%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -3,9 +3,7 @@ class Solution:
         L = []
         
         for x in nums:
-            if not L or L[-1] < x:
-                L.append(x)
-            elif L[0] >= x:
-                L[0] = x
+            if not L or L[-1] < x: L.append(x)
+            elif L[0] >= x: L[0] = x
             else: L[1] = x
         return len(L) > 2
