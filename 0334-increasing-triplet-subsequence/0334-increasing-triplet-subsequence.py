4b85f52000bd7cd5a381444ead100dd6e8e23a5d
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-04 14:41:52 +0200

    Time: 2566 ms (10.95%), Space: 29 MB (13.54%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        L = []
+        
+        for x in nums:
+            if not L or L[-1] < x:
+                L.append(x)
+            elif L[0] >= x:
+                L[0] = x
+            else: L[1] = x
+        return len(L) > 2
