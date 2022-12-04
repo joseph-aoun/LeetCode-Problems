d84e65109b93e23dedf6aac0f2ac31bc1d97427c
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-04 19:32:27 +0200

    Time: 265 ms (70.28%), Space: 14.9 MB (55.98%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        c = Counter(s)
+        L = sorted([c[x] for x in c.keys()], reverse = True)
+        ans= 0
+        
+        print(L)
+        
+        for i in range(1, len(L)):
+            if L[i] >= L[i-1]:
+                if L[i - 1] != 0:
+                    ans += L[i] - L[i-1] + 1
+                    L[i] = max(0, L[i-1]-1)
+                else:
+                    ans += L[i]
+                    L[i] = 0
+        return ans
