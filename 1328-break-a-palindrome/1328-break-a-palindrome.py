88d029957327582b5951649dff1f7f04e6a3e2ed
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-04 15:46:37 +0200

    Time: 51 ms (56.96%), Space: 14.1 MB (14.45%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -3,11 +3,10 @@ class Solution:
         if len(palindrome) == 1: return ''
         palindrome = list(palindrome)
         
-        for i, x in enumerate(palindrome):
+        for i in range(len(palindrome)//2):
             if i >= len(palindrome)//2: break
             if palindrome[i] != "a":
                 palindrome[i] = "a"
                 return "".join(palindrome)
-            
         palindrome[-1] = "b"
         return "".join(palindrome)
