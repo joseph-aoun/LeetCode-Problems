24b07120d1ac847871ad529fe02bece378207172
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2022-12-04 14:45:57 +0200

    Time: 357 ms (5.03%), Space: 109.6 MB (7.82%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -4,15 +4,9 @@ public:
         vector<int>L;
         
         for (auto &x : nums){
-            if (L.empty() || L[L.size() - 1] < x){
-                L.push_back(x);
-            }
-            else if(L[0] >= x){
-                L[0] = x;
-            }
-            else{
-                L[1] = x;
-            }
+            if (L.empty() || L[L.size() - 1] < x) L.push_back(x);
+            else if(L[0] >= x) L[0] = x;
+            else L[1] = x;
             if(L.size() > 2) return true;
         }
         return L.size() > 2;
