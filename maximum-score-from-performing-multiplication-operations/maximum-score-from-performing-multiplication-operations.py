8ae4869fefa76c4bf3605a01cb44225a65f4a250
joseph-aoun <106529665+joseph-aoun@users.noreply.github.com>
2023-09-30 11:44:56 +0300

    Time: 1356 ms, Memory: 166.7 MB - LeetHub

--- maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.py
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.py
@@ -2,12 +2,12 @@ class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
         n, m = len(nums), len(multipliers)
         dp = {}
-        def maxi(i, j, i1):
-            if i > i1 or j >= m:
+        def maxi(i, j):
+            if i > j or n-j-1+i >= m:
                 return 0
-            if (i, i1, j) in dp:
-                return dp[(i, i1, j)]
-            ans =  max(nums[i]*multipliers[j] + maxi(i+1, j+1, i1), nums[i1]*multipliers[j] + maxi(i, j+1, i1-1))
-            dp[(i, i1, j)] = ans
+            if (i, j) in dp:
+                return dp[(i, j)]
+            ans =  max(nums[i]*multipliers[i+n-j-1] + maxi(i+1, j), nums[j]*multipliers[n-j-1+i] + maxi(i, j-1))
+            dp[(i, j)] = ans
             return ans
-        return maxi(0, 0, n-1)
+        return maxi(0, n-1)
